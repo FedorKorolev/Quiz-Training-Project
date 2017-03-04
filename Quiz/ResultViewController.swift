caf692c92ca1e111a7d8f4cf85660137fab4a957
FedorKorolev <korolev.f@icloud.com>
2017-03-04 16:07:09 +0300

    Added result view

---
+++ Quiz/ResultViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by Фёдор Королёв on 04.03.17.
+//  Copyright © 2017 Фёдор Королёв. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    
+    var score = 0 //От 0 до 100
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
