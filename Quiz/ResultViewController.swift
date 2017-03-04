9a846848cf22d10e45090d01b686575cc6dca340
FedorKorolev <korolev.f@icloud.com>
2017-03-04 17:08:52 +0300

    Result screen improved

--- Quiz/ResultViewController.swift
+++ Quiz/ResultViewController.swift
@@ -14,28 +14,40 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     
     
-    var score = 0 //От 0 до 100
-    
+    var score = 0 {//От 0 до 100
+        didSet {
+            if score < 0 {
+                score = 0
+            }
+            if score > 100 {
+                score = 100
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        resultLabel.text = "\(score)"
+        
+        var text = ""
+        
+        switch score {
+        case 0:         text = "Похоже, вы бот"
+        case 1...30:    text = "Повезёт в другой раз"
+        case 31...50:   text = "Не плохо, но и не хорошо"
+        case 51...99:   text = "Отлично!"
+        default:        text = "Вы точно бот"
+        }
+        
+        resultLabel.text = "\(score)\n\(text)"
+        
+        progressView.setProgress((Float(score)/100), animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
